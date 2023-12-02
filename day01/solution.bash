d7777f2ff0412d64b611814864148f9b661b9df8
u08120fh <feroz7hassan@gmail.com>
2023-12-02 14:31:30 +0000

    fixed all bugs

--- day01/solution.bash
+++ day01/solution.bash
@@ -6,7 +6,7 @@ for line in `cat input.txt`; do
     # Part 1
     num1=$(echo $line | grep -op '\d' | sed -n '1p;$p' | tr -d '\n')
     p1=$((p1 + num1));
-    # Part 2 (Fixed bug by adding global replacement! May still contain another bug)
+    # Part 2
     l2=$(echo $line | sed -e 's/one/o1ne/g;s/two/t2wo/g;s/three/t3hree/g;s/four/f4our/g;s/five/f5ive/g;s/six/s6ix/g;s/seven/s7even/g;s/eight/e8ight/g;s/nine/n9ine/g')
     num2=$(echo $l2 | grep -op '\d' | sed -n '1p;$p' | tr -d '\n')
     p2=$((p2 + num2));
