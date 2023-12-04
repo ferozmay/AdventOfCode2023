3b5ba07692baf8d53ac12530d46eaee325fba9a0
u08120fh <feroz7hassan@gmail.com>
2023-12-04 08:21:47 +0000

    Solved p1, p2 in bash

--- day04/solution.bash
+++ day04/solution.bash
@@ -1,25 +1,25 @@
 #!bin/bash
 
-p1=0
-len=$(($(wc -l < input.txt) + 1))
-cards=($(yes 1 | head -n $len))
-i=0
+p1=0;
+len=$(($(wc -l < input.txt) + 1));
+cards=($(yes 1 | head -n $len));
+i=0;
 while read -r line; do
     # Part 1
-    left=$(echo "$line" | awk -F"[:|]" '{printf $2}' | tr ' ' '\n')
-    right=$(echo "$line" | awk -F"[:|]" '{printf $3}' | tr ' ' '\n')
-    common=$(comm -12 <(echo "$left"| sort) <(echo "$right" | sort) | wc -w)
-    if ((common));then
-        p1=$((p1 + 2 ** (common-1)))
+    left=$(echo "$line" | awk -F"[:|]" '{printf $2}' | tr ' ' '\n');
+    right=$(echo "$line" | awk -F"[:|]" '{printf $3}' | tr ' ' '\n');
+    common=$(comm -12 <(echo "$left"| sort) <(echo "$right" | sort) | wc -w);
+    if ((common)); then
+        p1=$((p1 + 2 ** (common-1)));
     fi;
     # Part 2
     for ((j=0; j<common; j++)); do
-        ((cards[$((i+j+1))]+=cards[$i]))
-    done
-    ((i++))
-done < input.txt
+        ((cards[$((i+j+1))]+=cards[$i]));
+    done;
+    ((i++));
+done < input.txt;
 
-p2=$(echo "${cards[@]}" | tr ' ' '+' | bc)
+p2=$(echo "${cards[@]}" | tr ' ' '+' | bc);
 
 echo "Part 1: $p1";
 echo "Part 2: $p2";
